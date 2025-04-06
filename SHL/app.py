98d274f1506f1256666fe60d9040e19fc3d78f15
Iris <168291438+Iris3014@users.noreply.github.com>
2025-04-06 13:50:14 +0530

    Update app.py

--- SHL/app.py
+++ SHL/app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="SHL GenAI Assessment Recommender", layout="wide")
 
     @st.cache_data
 def load_data():
-    return pd.read_csv("SHL\datasets\shl_catalog.csv")
+    return pd.read_csv("SHL/datasets/shl_catalog.csv")
 # Load embedding model
 @st.cache_resource
 def load_local_model():
